f9eebd1949cd8bcf8741111d619a26951d7c7bc3
Narsiles <92451486+Narsiles@users.noreply.github.com>
2022-05-10 02:27:47 +0200

    Oé UwU

---
+++ WillSmithUwU/WillSmith/Source.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include<iostream>
+#include <string>
+#include <map>
+
+using std::ifstream;
+using std::string;
+using namespace std;
+
+int main()
+{
+    ifstream ifs("musique.txt");
+    string m;
+    map<string, int> map;
+
+    while (ifs >> m)
+        ++map[m];
+
+    typedef std::map<string, int>::const_iterator iter;
+    for (iter it = map.begin(); it != map.end(); ++it)
+       cout << "le nombre d'occurence du mot " << it->first << " est " << it->second << endl;
+
+    return 0;
+}
